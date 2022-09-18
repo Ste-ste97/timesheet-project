8910c498195fc69c12b3714c8b866aa6061d8ec5
Demetris Kaizer <mrdkaizer@gmail.com>
2022-09-18 13:50:24 +0300

    fix cs in policy

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -5,66 +5,29 @@ namespace App\Policies;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class RolePolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
+    public function viewAny(User $user): bool {
         return $user->hasPermissionTo('roles.view');
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Role $role)
-    {
+    public function view(User $user, Role $role): bool {
         return $user->hasPermissionTo('roles.view');
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
+    public function create(User $user): bool {
         return $user->hasPermissionTo('roles.create');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Role $role)
-    {
+    public function update(User $user, Role $role): bool {
         return $user->hasPermissionTo('roles.edit');
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Role $role)
-    {
+    public function delete(User $user, Role $role): bool {
         return $user->hasPermissionTo('roles.delete');
     }
 
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -4,66 +4,29 @@ namespace App\Policies;
 
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class UserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
+    public function viewAny(User $user): bool {
         return $user->hasPermissionTo('users.view');
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, User $model)
-    {
+    public function view(User $user, User $model): bool {
         return $user->hasPermissionTo('users.view');
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
+    public function create(User $user): bool {
         return $user->hasPermissionTo('users.create');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, User $model)
-    {
+    public function update(User $user, User $model): bool {
         return $user->hasPermissionTo('users.edit');
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, User $model)
-    {
+    public function delete(User $user, User $model): bool {
         return $user->hasPermissionTo('users.delete');
     }
 
