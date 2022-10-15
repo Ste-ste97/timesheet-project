f0a2a4b3a8c86617140e7f0147ae6c588009e851
Demetris Kaizer <mrdkaizer@gmail.com>
2022-10-15 03:12:26 +0300

    cs

--- resources/js/Components/NavItem.vue
+++ resources/js/Components/NavItem.vue
@@ -1,9 +1,11 @@
 <template>
     <li>
         <NavLink v-has-permission="{props: $page.props, permissions: permissions?.split('|')}" :children="children" :href="route_name" :icon="icon" :name="name">
-            <NavLink v-for="(child,index) in children" :key="index+child.name" v-has-permission="{props: $page.props, permissions: child.permissions?.split('|')}" :children="child.children" :href="child.route_name" :icon="child.icon"
+            <NavLink v-for="(child,index) in children" :key="index+child.name"
+                     v-has-permission="{props: $page.props, permissions: child.permissions?.split('|')}" :children="child.children" :href="child.route_name" :icon="child.icon"
                      :name="child.name">
-                <NavLink v-for="(grandchild,index) in child.children" :key="index+grandchild.name" v-has-permission="{props: $page.props, permissions: grandchild.permissions?.split('|')}" :children="[]" :href="grandchild.route_name" :icon="grandchild.icon"
+                <NavLink v-for="(grandchild,index) in child.children" :key="index+grandchild.name"
+                         v-has-permission="{props: $page.props, permissions: grandchild.permissions?.split('|')}" :children="[]" :href="grandchild.route_name" :icon="grandchild.icon"
                          :name="grandchild.name"></NavLink>
             </NavLink>
         </NavLink>
