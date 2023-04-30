2361aa5244159606d76453e3f6a9217b191492e6
erotokritoscy <erotokritos.cy@gmail.com>
2023-04-30 17:50:41 +0300

    auto set docker uuid and user

--- install_app.sh
+++ install_app.sh
@@ -2,13 +2,21 @@
 echo -n "This script is about to rebuild the project. All data will be lost are you sure? (y/n)"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
+    cp .env.docker .env
+    # check if .env file not contains DOCKER_UID
+    if ! grep -q "DOCKER_UID" .env; then
+        echo "DOCKER_UID=$(id -u)" >> .env
+    fi
+    # check if .env file not contains DOCKER_USER
+    if ! grep -q "DOCKER_USER" .env; then
+        echo "DOCKER_USER=$(whoami)" >> .env
+    fi
     docker-compose build
     docker-compose up -d
     echo "Done with docker"
     sudo chmod u+x composer.sh
     ./composer.sh install
     sudo chmod o+w -R storage/
-    cp .env.docker .env
     sudo chmod u+x artisan.sh
     ./artisan.sh key:generate
     sudo chmod u+x yarn.sh
